388ae3778c170aa2c6f663471e60ee6c16e2eb78
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-10 11:52:46 -0500

    feat(public): add footer component.

---
+++ src/public/components/footer.component.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__content">
+      <p>© 2021 - All rights reserved</p>
+      <h2 class="footer__author">Developed by 202113 Gonzalo Quedena</h2>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+.footer {
+  background-color: darkkhaki;
+  padding: 50px;
+}
+
+.footer__content {
+  text-align: center;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.footer__author {
+  padding-top: 20px ;
+}
+
+</style>
