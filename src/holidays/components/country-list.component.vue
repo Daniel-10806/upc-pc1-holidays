7c1f7c0468f2ff1209774d5ce9cc8c945e4bc1e6
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-10 11:23:52 -0500

    chore(holidays): add country list component.

--- src/holidays/components/country-list.component.vue
+++ src/holidays/components/country-list.component.vue
@@ -1,10 +1,33 @@
 <script>
+import CountryItem from './country-item.component.vue'
+
 export default {
-  name: "country-list.component"
+  name: "country-list",
+
+  components: {
+    CountryItem
+  },
+
+  props: {
+    countries: {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 
 <template>
+
+  <div v-if="countries.length">
+    <div v-for="country in countries" >
+      <country-item :country="country" :key="country.uuid"/>
+    </div>
+  </div>
+
+  <div v-else>
+    <p>No countries available.</p>
+  </div>
 
 </template>
 
