a5a9d1b781025ffed59fa3bfa14a443d1df162d5
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-10 11:12:00 -0500

    feat(holidays): add country item component.

---
+++ src/holidays/components/country-item.vue
@@ -0,0 +1,36 @@
+<script>
+import {Country} from "../model/country.entity.js";
+
+export default {
+  name: "country-item",
+
+  props: {
+    country: {
+      type: Country,
+      required: true
+    }
+  },
+
+}
+</script>
+
+<template>
+
+  <pv-card style="width: 25rem; overflow: hidden">
+    <template #header>
+      <h1>{{ country.countryName }}</h1>
+    </template>
+    <template #title>{{ country.iso3166 }}</template>
+    <template #subtitle>{{ country.totalHolidays }}</template>
+    <template #content>
+      <p class="m-0">
+        {{ country.supportedLanguages }} {{ country.uuid }} {{ country.flagUnicode }}
+      </p>
+    </template>
+  </pv-card>
+
+</template>
+
+<style scoped>
+
+</style>
