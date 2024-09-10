329d50fdc437bb2f0e6a03f2309e1c84567b12f9
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-10 10:34:35 -0500

    feat: add primevue configuration and their components to be used.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import Toolbar from 'primevue/toolbar';
+import Card from 'primevue/card';
+
 const app = createApp(App);
+
+app.use(PrimeVue, {
+    ripple: true, theme: {
+        preset: Aura, options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.component('pv-toolvar', Toolbar);
+app.component('pv-card', Card);
+
 app.mount('#app');
